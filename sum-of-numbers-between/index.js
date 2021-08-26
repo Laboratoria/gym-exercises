e74a80141575473ccabd6323f8208d093b3c1bd5
merunga <merunga@gmail.com>
2021-08-26 15:11:38 +0200

    skip tests for N != 200

--- sum-of-numbers-between/index.js
+++ sum-of-numbers-between/index.js
@@ -1,10 +1,5 @@
-const sumOfNumbers = (num = 200) => {
+const sumOfNumbers = () => {
   // tu codigo aqui
-  let result = 0;
-  for(let i = 1; i <= num; i++) {
-    result = result + i;
-  }
-  return result;
 };
 
 module.exports.sumOfNumbers = sumOfNumbers;
--- sum-of-numbers-between/index.spec.js
+++ sum-of-numbers-between/index.spec.js
@@ -5,7 +5,7 @@ describe("Sum of number between 1 and N", () => {
     expect(sumOfNumbers()).toBe(20100);
   });
 
-  it('debería retornar 500500 para n = 1000', () => {
+  it.skip('debería retornar 500500 para n = 1000', () => {
     expect(sumOfNumbers(1000)).toBe(500500);
   });
 });
