d93e5af90df6ca84b6f2c84e5826d62911616de8
merunga <merunga@gmail.com>
2021-08-26 15:09:36 +0200

    agregar link a youtube

--- sum-of-numbers-between/README.md
+++ sum-of-numbers-between/README.md
@@ -1,3 +1,5 @@
 # Sum of number between 1 and 200
 
 ¿Cuál es la suma de todos los números comprendidos entre 1 y 200?
+
+Extraído de [www.youtube.com/watch?v=qbnTZCj0ugI](https://www.youtube.com/watch?v=qbnTZCj0ugI)
--- sum-of-numbers-between/index.js
+++ sum-of-numbers-between/index.js
@@ -1,5 +1,10 @@
 const sumOfNumbers = (num = 200) => {
   // tu codigo aqui
+  let result = 0;
+  for(let i = 1; i <= num; i++) {
+    result = result + i;
+  }
+  return result;
 };
 
 module.exports.sumOfNumbers = sumOfNumbers;
