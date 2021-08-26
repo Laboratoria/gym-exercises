3577279fd7afa0098711ec6201b117482d409d94
merunga <merunga@gmail.com>
2021-08-26 17:05:13 +0200

    mas test cases

--- sum-of-numbers-between/index.spec.js
+++ sum-of-numbers-between/index.spec.js
@@ -1,8 +1,16 @@
 const { sumOfNumbers } = require(".");
 
 describe("Sum of number between 1 and N", () => {
-  it('debería retornar 20100', () => {
+  it('debería retornar 20100 por defecto o n = 200', () => {
     expect(sumOfNumbers()).toBe(20100);
+  });
+
+  it.skip('debería retornar 55 para n = 10', () => {
+    expect(sumOfNumbers(10)).toBe(55);
+  });
+
+  it.skip('debería retornar 21 para n = 10', () => {
+    expect(sumOfNumbers(10)).toBe(55);
   });
 
   it.skip('debería retornar 500500 para n = 1000', () => {
