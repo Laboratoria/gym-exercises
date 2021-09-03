828b738ebd80211c157ef50486ef8ef2d1eaebf7
merunga <merunga@gmail.com>
2021-09-03 23:52:55 +0200

    quitar skips

--- sum-of-numbers-between/index.spec.js
+++ sum-of-numbers-between/index.spec.js
@@ -5,23 +5,23 @@ describe("Sum of number between 1 and N", () => {
     expect(sumOfNumbers()).toBe(20100);
   });
 
-  it.skip('debería retornar 55 para n = 10', () => {
+  it('debería retornar 55 para n = 10', () => {
     expect(sumOfNumbers(10)).toBe(55);
   });
 
-  it.skip('debería retornar 21 para n = 6', () => {
+  it('debería retornar 21 para n = 6', () => {
     expect(sumOfNumbers(6)).toBe(21);
   });
 
-  it.skip('debería retornar 28 para n = 7', () => {
+  it('debería retornar 28 para n = 7', () => {
     expect(sumOfNumbers(7)).toBe(28);
   });
 
-  it.skip('debería retornar 144453 para n = 537', () => {
+  it('debería retornar 144453 para n = 537', () => {
     expect(sumOfNumbers(537)).toBe(144453);
   });
 
-  it.skip('debería retornar 500500 para n = 1000', () => {
+  it('debería retornar 500500 para n = 1000', () => {
     expect(sumOfNumbers(1000)).toBe(500500);
   });
 });
